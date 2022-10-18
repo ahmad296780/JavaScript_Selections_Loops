4a212dab45d9f7c715efb532361c1f5131f4a42e
ahmad <bha296780@gmail.com>
2022-10-18 12:01:45 -0400

    final

--- app.js
+++ app.js
@@ -1,7 +1,77 @@
-console.log("Hello World!\n==========\n");
+for (let i = 0; i < 101; i++) {
 
-// Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+    if (i % 2 != 0) {
+        console.log(i);
+    }
 
-// Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+}
+// Exercise2
+// for (let i = 0; i < 101; i++) {
+//     console.log(i)
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log("BOTH")
+
+//     } else {
+//         if (i % 5 == 0) {
+//             console.log("div by 5")
+//         } else {
+//             if (i % 3 == 0) {
+//                 console.log("div by 3")
+//             }
+
+//         }
+
+//     }
+
+// } 
+// i=0;
+// while (i<100) {
+//     console.log(i)
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log("BOTH")
+
+//     } else {
+//         if (i % 5 == 0) {
+//             console.log("div by 5")
+//         } else {
+//             if (i % 3 == 0) {
+//                 console.log("div by 3")
+//             }
+
+//         }
+
+//     }
+// i++
+// }
+
+// i=1;
+// do {console.log(i)
+//     if (i % 3 == 0 && i % 5 == 0) {
+//         console.log("BOTH")
+
+//     } else {
+//         if (i % 5 == 0) {
+//             console.log("div by 5")
+//         } else {
+//             if (i % 3 == 0) {
+//                 console.log("div by 3")
+//             }
+
+//         }
+
+//     }
+
+//     i++
+// } while (i < 101);
+
+let value = Math.round((Math.random() * 500)); // creates a random number between 0 and 500
+let n = Math.round(Math.random() * (500 - 100) + 100); // creates a random number between 100 and 500
+for (let i = 0; i < n; i++) {
+    if (i=value) {
+        console.log("find value")
+        break;
+    } else {
+        console.log("did not find value")
+    }
+    
+}
